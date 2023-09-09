4f2b87ae1f74b4f34231863a82aaa4d0ddd6c7b3
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-09 13:39:42 +0100

    Initial Commit

---
+++ Apple-Frameworks/GridView.swift
@@ -0,0 +1,29 @@
+//
+//  GridView.swift
+//  Apple-Frameworks
+//
+//  Created by robin tetley on 09/09/2023.
+//
+
+import SwiftUI
+
+struct GridView: View {
+    var body: some View {
+        VStack {
+            Image("arkit")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 80, height: 80)
+            Text("ARKit")
+                .font(.system(size: 28, weight: .medium))
+            
+        }
+    }
+}
+
+struct GridView_Previews: PreviewProvider {
+    static var previews: some View {
+        GridView()
+    }
+}
