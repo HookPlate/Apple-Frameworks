d5142355a2de0dacf38ef4b6a9ef3f7a364f0336
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-10 09:15:43 +0100

    Quick commit before I go to the lorries

---
+++ Apple-Frameworks/FrameworkGridViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FrameworkGridViewModel.swift
+//  Apple-Frameworks
+//
+//  Created by robin tetley on 10/09/2023.
+//
+
+import Foundation
+
+final class FrameworkGridViewModel: ObservableObject {
+    
+    var selectedFramework : Framework?
+}
